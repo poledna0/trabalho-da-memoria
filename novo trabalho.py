831a2cbdeaa6aed26d2eee46b37065bb3e03be65
poledna0 <henrique.poledna@gmail.com>
2024-06-06 14:15:46 -0300

    feat: sla cara

--- novo trabalho.py
+++ novo trabalho.py
@@ -1,12 +1,24 @@
 import random
 
-def quadrante(dificul):
+def quadrante(dificul): # pensei em ainda deixar em ascii e fazer um ocntador que vai colocar a mesma letra se o contador estiver em 1, quando chegar em dois ele gera outro numero ascii
+    alfabeto_aleatorio = []
+    while len(alfabeto_aleatorio) < 26:
+        numero_aleatorio= random.randint(65,90)
+        if existe(alfabeto_aleatorio, numero_aleatorio):
+            None
+        else:
+            alfabeto_aleatorio.append(numero_aleatorio)
+    for indice, numero in enumerate (alfabeto_aleatorio):
+        alfabeto_aleatorio[indice] = chr(numero)
 
-    quadrante = [[0 for _ in range()] for _ in range(dificul)]
+    print(alfabeto_aleatorio)
+def existe (vetor,numero):
+    for n in vetor:
+        if n == numero:
+            return True
+    return False
 
-    for i in range(dificul):
-        for j in range(dificul):
-            quadrante[i][j] = random.randint(1, 100) 
+
 
 def ler_entrada():
     vetor = []
@@ -17,20 +29,20 @@ def ler_entrada():
         vetor.append(coluna_entrada)
     return vetor
 
-
-
 if __name__ == "__main__":
-    escolhendo_a_dificulade = int(input('Digite a dificulade: facil-1, medio-2, dificil-3'))
-    if escolhendo_a_dificulade == 1:
-        dificul_escolhida = 4
 
-    elif escolhendo_a_dificulade == 2:
-        dificul_escolhida = 6
+    # escolhendo_a_dificulade = int(input('Digite a dificulade: facil-1 , medio-2 , dificil-3: '))
 
-    else:
-        dificul_escolhida = 8
-    quadrante = [list(range(dificul_escolhida))for _ in range(dificul_escolhida)]
-# cmc do cod        
-    while True:
-        ler_entrada()
+    # if escolhendo_a_dificulade == 1:
+    #     dificul_escolhida = 4
+    # elif escolhendo_a_dificulade == 2:
+    #     dificul_escolhida = 6
+    # else:
+    #     dificul_escolhida = 8
 
+    # quadrante(dificul_escolhida)
+    quadrante(4)
+
+    #while True:
+        #ler_entrada()
+        
